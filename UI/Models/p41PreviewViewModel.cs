987e918de6bfb4a1d5f278a83c6807e8f035fa35
Jiri Theimer <jiri.theimer@gmail.com>
2020-05-24 13:51:51 +0200

    a

--- UI/Models/p41PreviewViewModel.cs
+++ UI/Models/p41PreviewViewModel.cs
@@ -8,5 +8,7 @@ namespace UI.Models
     public class p41PreviewViewModel:BaseViewModel
     {
         public BO.p41Task Rec { get; set; }
+        public BO.p52OrderItem RecP52 { get; set; }
+        public BO.p51Order RecP51 { get; set; }
     }
 }
