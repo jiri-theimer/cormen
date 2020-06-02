2e5a3fcb41ae89696b1b01c5e4067c61be34d17a
Jiri Theimer <jiri.theimer@gmail.com>
2020-06-03 01:30:35 +0200

    a

--- UI/Controllers/o53Controller.cs
+++ UI/Controllers/o53Controller.cs
@@ -33,8 +33,8 @@ namespace UI.Controllers
                
             }
 
-            v.Toolbar = new MyToolbarViewModel(v.Rec);            
-           
+            v.Toolbar = new MyToolbarViewModel(v.Rec);
+            if (isclone) { v.Toolbar.MakeClone(); }
             return View(v);
         }
         [HttpPost]
